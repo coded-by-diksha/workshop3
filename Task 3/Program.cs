b0bb15acd39c750c4ddcabc4c55033b3f7327bfd
Dikshaaa1212 <ghimirediksha222@gmail.com>
2025-11-12 11:47:52 +0545

    task 5 done

---
+++ Task 3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_3
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 13)
+            {
+                Console.WriteLine("Child.");
+            }
+            else if (age >= 13 && age < 20)
+            {
+                Console.WriteLine("Teenager.");
+            }
+            else if (age >= 20 && age < 60)
+            {
+                Console.WriteLine("Adult.");
+            }
+            else
+            {
+                Console.WriteLine("You are a senior citizen.");
+            }
+        }
+
+     
+    }
+}
---
+++ Task 5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("input the value of N:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum=0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i;
+
+
+            }
+            Console.WriteLine(sum);
+            int i = 1;
+            while ( i<20){
+              
+                 if(i%4==0) {
+                    continue;
+                }
+                else if (i == 15)
+                {
+                    break;
+                }
+                 Console.WriteLine(i);
+            }
+
+            // Part 3: Sum of all elements in an array using foreach loop
+            int[] arr = { 2, 4, 6, 8, 10 }; // Example array
+            int total = 0;
+            foreach (int item in arr)
+            {
+                total += item;
+            }
+            Console.WriteLine($"Sum of all elements in the array is: {total}");
+        }
+    }
+
+}
+
+   
