e0c6f0a1b7f5927402f04530604d4d81583e5378
Dikshaaa1212 <ghimirediksha222@gmail.com>
2025-11-12 11:05:01 +0545

    Task 1 done

---
+++ Task 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using static Task_1.@operator;
+
+namespace Task_1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+    {
+        Operators op = new Operators();
+
+        op.Add(10, 5);
+        op.Subtract(15, 5);
+        op.Multiply(99, 5);
+        op.Divide(555, 5);
+        op.OddEvenFinder(956);
+
+        Console.ReadLine(); // Keeps console open
+    }
+}
+
+
+}
